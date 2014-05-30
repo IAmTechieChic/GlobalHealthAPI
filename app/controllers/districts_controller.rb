dc3c8c8398ada75b5e4292281d000d56c18baddc
Jaime <jpanaia@gmail.com>
2014-05-30 09:43:35 -0400

    updated login flow

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,6 +1,8 @@
 class DistrictsController < ApplicationController
   before_action :set_district, only: [:show, :edit, :update, :destroy]
 
+  before_filter :authenticate_user!
+
   # GET /districts
   # GET /districts.json
   def index
